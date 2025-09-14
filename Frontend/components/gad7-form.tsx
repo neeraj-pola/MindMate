09267aae670a9a923971de853be44a8e2be5530d
neeraj-pola <neerajpoladen@gmail.com>
2025-09-14 00:45:35 -0400

    updated backend server link for 2 forms

--- Frontend/components/gad7-form.tsx
+++ Frontend/components/gad7-form.tsx
@@ -75,7 +75,7 @@ export function GAD7Form({ onClose }: GAD7FormProps) {
   const submitAssessment = async () => {
     setLoading(true);
     try {
-      const res = await fetch("http://127.0.0.1:8000/assess", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/assess`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
--- Frontend/components/phq9-form.tsx
+++ Frontend/components/phq9-form.tsx
@@ -65,7 +65,7 @@ export function PHQ9Form({ onClose }: PHQ9FormProps) {
   const submitAssessment = async () => {
     setLoading(true)
     try {
-      const res = await fetch("http://127.0.0.1:8000/assess", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/assess`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
